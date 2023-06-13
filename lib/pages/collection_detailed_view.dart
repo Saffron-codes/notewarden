37662ed5a0b3f6977bf496de53129db4c5de9a64
Saffron-codes <saffrontdionysius@gmail.com>
2023-06-13 22:23:35 +0530

    added media loading into detailed view

--- lib/pages/collection_detailed_view.dart
+++ lib/pages/collection_detailed_view.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:note_warden/providers/collection_provider.dart';
+import 'package:note_warden/providers/media_provider.dart';
+import 'package:note_warden/utils/enums.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:provider/provider.dart';
 
 import '../models/collection_model.dart';
@@ -12,7 +17,6 @@ class CollectionDetailedView extends StatefulWidget {
 }
 
 class _CollectionDetailedViewState extends State<CollectionDetailedView> {
-
   Collection? collection;
 
   @override
@@ -21,6 +25,8 @@ class _CollectionDetailedViewState extends State<CollectionDetailedView> {
     WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
       Provider.of<CollectionProvider>(context, listen: false)
           .loadMediaCount(collectionId: collection!.id);
+      Provider.of<MediaProvider>(context, listen: false)
+          .loadMedia(collectionId: collection!.id);
     });
   }
 
@@ -42,17 +48,56 @@ class _CollectionDetailedViewState extends State<CollectionDetailedView> {
         actions: [
           Consumer<CollectionProvider>(
             builder: (context, viewModel, child) {
-              return Text(viewModel.mediaCount.toString());
+              return Container(
+                margin: const EdgeInsets.only(right: 10),
+                child: Text(viewModel.mediaCount.toString(),style: TextStyle(fontWeight: FontWeight.bold),),
+              );
             },
           )
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () async {
+          final result = await Permission.manageExternalStorage.request();
+          print(result);
+          // openAppSettings();
+        },
         child: const Icon(
           Icons.add_a_photo_rounded,
         ),
       ),
+      body: Consumer<MediaProvider>(builder: (context, viewModel, child) {
+        if (viewModel.loadMediaState == TaskState.loading) {
+          return Center(child: CircularProgressIndicator());
+        } else if (viewModel.loadMediaState == TaskState.success) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing: 10.0,
+                crossAxisSpacing: 10.0,
+                childAspectRatio: 1.0,
+              ),
+              itemCount: viewModel.media.length,
+              itemBuilder: (context, index) {
+                return Container(
+                    height: 200.0, // Fixed height for the card
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: FileImage(File(viewModel.media[index]
+                            .location)), // Replace with your image path
+                        fit: BoxFit.cover,
+                      ),
+                      borderRadius: BorderRadius.circular(10.0),
+                    ));
+              },
+            ),
+          );
+        } else {
+          return Text("None");
+        }
+      }),
     );
   }
 }
